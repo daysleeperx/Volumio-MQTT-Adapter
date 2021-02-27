7802aaf85a4b5717c8aefc49e758c1d8a0d26d26
Viktor Pavlov <vvpavlov19@gmail.com>
2021-02-27 22:02:45 +0200

    add more socketio event handlers

--- adapter.py
+++ adapter.py
@@ -43,6 +43,9 @@ def subscribe(client: mqtt.Client, socket: socketio.Client):
 
     volume_options = {
         'percent': lambda msg: socket.emit('volume', int(msg)),
+        'push': lambda msg: socket.emit('volume', msg),
+        'up': lambda _: socket.emit('volume', '+'),
+        'down': lambda _: socket.emit('volume', '-'),
         'mute': lambda msg:
             socket.emit('volume', 'mute' if msg == 'true' else 'unmute')
     }
@@ -50,14 +53,18 @@ def subscribe(client: mqtt.Client, socket: socketio.Client):
     playback_options = {
         'play': lambda _: socket.emit('play'),
         'stop': lambda _: socket.emit('stop'),
-        'pause': lambda _: socket.emit('pause')
+        'pause': lambda _: socket.emit('pause'),
+        'playPlaylist': lambda msg:
+            socket.emit('playPlaylist', {'name': msg}),
+        'power': lambda msg:
+            socket.emit('play' if msg == 'true' else 'stop')
     }
 
     options = {
         'playback': lambda cmd, _:
-            playback_options.get(cmd, lambda: log(f'No such cmd: {cmd}'))(_),
+            playback_options.get(cmd, lambda _: log(f'No such cmd: {cmd}'))(_),
         'volume': lambda cmd, msg:
-            volume_options.get(cmd, lambda: log(f'No such cmd: {cmd}'))(msg)
+            volume_options.get(cmd, lambda _: log(f'No such cmd: {cmd}'))(msg)
     }
 
     def on_message(_, userdata, msg):
@@ -66,7 +73,11 @@ def subscribe(client: mqtt.Client, socket: socketio.Client):
             log(f'Message received from MQTT topic {msg.topic}: {message}')
 
         (_, action, command) = msg.topic.split('/')
-        options.get(action, lambda act: log(f'No such action: {act}'))(command, message)
+
+        try:
+            options.get(action, lambda c, m: log(f'No such action: {action}'))(command, message)
+        except Exception as e:
+            log(f'Exception occurred while executing command: {e}')
 
     client.subscribe(MQTT_TOPIC)
     client.on_message = on_message

d9ed1acd2b8efb4e7fe34000f1cb44ee5c25f0ec
Viktor Pavlov <vvpavlov19@gmail.com>
2021-02-28 20:41:22 +0200

    clean up CI scripts

--- docker_start.sh
+++ docker_start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-docker build --tag volumio-mqtt-docker .
-docker tag volumio-mqtt-docker:latest volumio-mqtt-docker:v1.0.0
+docker build --tag volumio-mqtt-docker:latest --tag volumio-mqtt-docker:v1.0.0 .
 docker stop volumio_mqtt || true && docker rm volumio_mqtt || true
-docker run -d --network host --name volumio_mqtt --restart unless-stopped \
-        --env IS_DEBUG=$CI_DEBUG \
-        --env VOLUMIO_HOST=$CI_VOLUMIO_HOST \
-        --env MQTT_HOST=$CI_MQTT_HOST \
-        --env MQTT_DEVICE=$CI_DEVICE volumio-mqtt-docker
+docker run -d --network host --name volumio_mqtt --restart unless-stopped volumio-mqtt-docker

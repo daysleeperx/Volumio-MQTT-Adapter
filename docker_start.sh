d2bdc5231d04846ffb3f1d52393167b114bec95b
Viktor Pavlov <vvpavlov19@gmail.com>
2021-02-28 16:23:39 +0200

    test gitlab-ci

--- docker_start.sh
+++ docker_start.sh
@@ -3,4 +3,4 @@
 docker build --tag volumio-mqtt-docker .
 docker tag volumio-mqtt-docker:latest volumio-mqtt-docker:v1.0.0
 docker stop volumio_mqtt || true && docker rm volumio_mqtt || true
-docker run -d --network host --name volumio_mqtt --restart unless-stopped volumio-mqtt-docker --env DEBUG=$CI_DEBUG --env VOLUMIO_HOST=$CI_VOLUMIO_HOST --env MQTT_HOST=$CI_MQTT_HOST --env DEVICE=CI_DEVICE
+docker run -d --network host --name volumio_mqtt --restart unless-stopped --env IS_DEBUG=$CI_DEBUG --env VOLUMIO_HOST=$CI_VOLUMIO_HOST --env MQTT_HOST=$CI_MQTT_HOST --env DEVICE=$CI_DEVICE volumio-mqtt-docker
